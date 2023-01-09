126feda8269b849d94e307852450a055f84e1f14
Toma1402 <toma.k1402@gmail.com>
2023-01-09 19:04:33 +0200

    i

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import user from '../data/user.json';
 import stats from '../data/data.json';
-import friends from '../data/friends.json';
+
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 
--- src/components/FriendList/FriendListItem.jsx
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-export const FriendListItem = ({ avatar, name, isOnline }) => (
-  <li>
-    <span></span>
-    <img src={avatar} alt={name} />
-    <p>{name}</p>
-  </li>
-);
